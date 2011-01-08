fbcebc7f8546fdbe564e7d5d647b0b683d98b215
Michał Sałaban <michal@salaban.info>
2011-01-08 22:00:37 +0100

    Always make the links absolute

--- djiki/parser.py
+++ djiki/parser.py
@@ -13,6 +13,23 @@ class DjikiHtmlEmitter(HtmlEmitter):
 		return u'<h%d>%s</h%d>\n' % (
 			node.level + 1, self.html_escape(node.content), node.level)
 
+	def link_emit(self, node):
+		target = node.content
+		if node.children:
+			inside = self.emit_children(node)
+		else:
+			inside = self.html_escape(target)
+		m = self.link_rules.addr_re.match(target)
+		if m:
+			if m.group('extern_addr'):
+				return u'<a href="%s">%s</a>' % (
+					self.attr_escape(target), inside)
+			elif m.group('inter_wiki'):
+				raise NotImplementedError
+		return u'<a href="%s">%s</a>' % (
+			reverse('djiki-page-view', kwargs={'title': utils.urlize_title(self.attr_escape(target))}),
+			inside)
+
 	def image_emit(self, node):
 		target = node.content
 		text = self.get_text(node)
