9622865985fa8ceb28da0d6be7c4be0e79bbba86
Michał Sałaban <michal@salaban.info>
2013-03-11 20:58:37 +0100

    Add contrib view of the latest changes

---
+++ djiki/contrib/views.py
@@ -0,0 +1,10 @@
+from django.db.models import Max
+from django.template.response import TemplateResponse
+
+from .. import models
+
+def latest_changes(request, limit=50):
+	latest = models.Page.objects\
+			.annotate(latest_change=Max('revisions__created'))\
+			.order_by('-latest_change')[:limit]
+	return TemplateResponse(request, 'djiki/latest_changes.html', {'latest': latest})
