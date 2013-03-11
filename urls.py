b4b70da2def11462ab4775d92045b000c3fd843c
Michał Sałaban <michal@salaban.info>
2013-03-11 20:58:55 +0100

    Introduce the latest changes view into the site

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
 	(r'^$', RedirectView.as_view(url=u'/wiki/Main_Page')),
 	(r'^wiki/', include('djiki.urls')),
+	url(r'^latest/', 'djiki.contrib.views.latest_changes', name='latest-changes'),
 	(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
 )
 
