c389a1c0b5312230c86a1a84db68f879f38f0a10
Michał Sałaban <michal@salaban.info>
2014-03-04 00:14:45 +0100

    Reduce title length to solve problems with mysql, close #10

--- djiki/models.py
+++ djiki/models.py
@@ -32,7 +32,7 @@ class Revision(models.Model):
 
 
 class Page(models.Model, Versioned):
-	title = models.CharField(_("Title"), max_length=256, unique=True)
+	title = models.CharField(_("Title"), max_length=255, unique=True)
 
 	class Meta:
 		ordering = ('title',)
